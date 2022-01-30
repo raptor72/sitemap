da7d2097d50fdc21d84465d6286a1eeacb320cf4
raptor72 <mctaras51@yandex.ru>
2022-01-30 22:55:42 +0400

    parsed url

--- main.go
+++ main.go
@@ -4,14 +4,26 @@ import (
 	"fmt"
 	"log"
 	"net/http"
-
+	"net/url"
 	"github.com/raptor72/glink"
 )
 
 
 func main() {
-	resp, err := http.Get("http://example.com/")
-    if err != nil {
+    // domain := "http://example.com/"
+	domain := "http://127.0.0.1:8080"
+
+	u, err := url.Parse(domain)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println(u.Scheme)
+	fmt.Println(u.Host)
+	fmt.Println(u.Path)
+
+	resp, err := http.Get(domain)
+	if err != nil {
 		log.Fatal(err)
 	}
     fmt.Println(resp)
@@ -24,4 +36,29 @@ func main() {
 	}
     fmt.Println(links)
 
+	for _, lnk := range links {
+
+
+		sub_u, err := url.Parse(lnk.Href)
+		if err != nil {
+			log.Fatal(err)
+		}
+        // print(sub_u.Path)
+        // print(sub_u.Scheme)
+		if sub_u.Scheme == sub_u.Host == nil {
+			resp, err := http.Get(u.Scheme + u.Host + sub_u.Path)
+			if err != nil {
+				log.Fatal(err)
+			}
+			fmt.Println(resp)
+		}
+
+		if sub_u.Host == u.Host && sub_u.Scheme == u.Scheme  {
+			resp, err := http.Get(sub_u.String())
+			if err != nil {
+				log.Fatal(err)
+			}
+			fmt.Println(resp)
+		}
+	}
 }
