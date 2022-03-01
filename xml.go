0c4b888956ce2866091a51dd5fa0f6bdce0add5b
raptor72 <mctaras51@yandex.ru>
2022-03-01 15:40:22 +0300

    correct xml handling

--- xml.go
+++ xml.go
@@ -1,7 +1,7 @@
 package main
 
 import (
-	// "os"
+	"os"
 	"fmt"
 	"encoding/xml"
 )
@@ -10,36 +10,25 @@ func main() {
     m := make(map[string]bool)
     m = map[string]bool{"/debate":true, "/denver":true, "/home":true, "/mark-bates":true, "/sean-kelly":true}
 
-
 	type Url struct {
 		XMLName   xml.Name `xml:"url"`
 		Loc       string   `xml:"loc"`
 	}
-
     type SiteMap struct {
-		XMLName   xml.Name `xml:"urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9""`
-		Url       []Url
+        XMLName   xml.Name `xml:"http://www.sitemaps.org/schemas/sitemap/0.9 urlset"`
+		Urls      []Url
 	}
-
-	fmt.Println(m)
-
 	v := &Url{}
-
-	var all_urls []*Url
+    var all_urls []Url
 	for key := range m {
         v.Loc = key
-		// xdata, err := xml.MarshalIndent(v, " ", "    ")
-		// if err != nil {
-			// fmt.Printf("error: %v\n", err)
-		// }
-		// os.Stdout.Write(xdata)
-        // fmt.Printf("%T\n", xdata)
-        all_urls = append(all_urls, v)
+        all_urls = append(all_urls, *v)
 	}
-    fmt.Println(all_urls)
-	sitemap := &SiteMap{Url: *all_urls}
-	// sitemap.Url = []*all_urls
-
-
-
+	sitemap := &SiteMap{Urls: all_urls}
+	xdata, err := xml.MarshalIndent(sitemap, " ", "    ")
+	if err != nil {
+		fmt.Printf("error: %v\n", err)
+	}
+    os.Stdout.Write([]byte(xml.Header))
+	os.Stdout.Write(xdata)
 }
